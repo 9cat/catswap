487a6282b90ab4ed065ecb3d43e7e5def98a1850
temple <you@example.com>
2020-11-19 17:38:30 +0000

    first commit

---
+++ src/constants/lists.ts
@@ -0,0 +1,8 @@
+// the Uniswap Default token list lives here
+export const DEFAULT_TOKEN_LIST_URL = 'tokens.uniswap.eth'
+
+export const DEFAULT_LIST_OF_LISTS: string[] = [
+  DEFAULT_TOKEN_LIST_URL,
+  'https://erc20.aib.finance/ropsten',
+  'https://umaproject.org/uma.tokenlist.json'
+]
